1d66eba4d197b930d38f10e5cb608b2726e63cd4
RAVI PATEL <ravipatel5047@gmail.com>
2018-04-08 12:03:03 +0530

    last commit

--- app/src/main/java/com/nitrocoders/healthhub/backpain.java
+++ app/src/main/java/com/nitrocoders/healthhub/backpain.java
@@ -2,6 +2,9 @@ package com.nitrocoders.healthhub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
 
 public class backpain extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class backpain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_backpain);
+
+
+
+        WebView webViewdip = (WebView)findViewById(R.id.webVideoViewdip);
+        webViewdip.setVisibility(View.VISIBLE);
+        String html ="<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/FiZ6ws8lYQk\" frameborder=\"0\" allow=\"autoplay; encrypted-media\" allowfullscreen></iframe>";
+
+        webViewdip.getSettings().setJavaScriptEnabled(true);
+        webViewdip.setWebChromeClient(new WebChromeClient() {
+        });
+
+        webViewdip.loadData(html, "text/html", "utf-8");
     }
+
 }

84693592b3db7f80541bc971c9a0a4186ca03519
neel-98 <neelbpatel98@gmail.com>
2018-04-08 16:08:59 +0530

    Revert "Merge branch 'master' of https://github.com/MrMit007/healthhub"
    This reverts commit ee55dfc2c274f316353d0bf9e5cfef9dd8faa5de, reversing
    changes made to 6c45c433266bbe8dcbc2b9727e408ef49c4e8742.

--- app/src/main/java/com/nitrocoders/healthhub/MainActivity.java
+++ app/src/main/java/com/nitrocoders/healthhub/MainActivity.java
@@ -1,7 +1,6 @@
 package com.nitrocoders.healthhub;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -78,10 +77,8 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
-            
         } else if (id == R.id.nav_gallery) {
-            Intent i = new Intent (MainActivity.this,Call.class);
-            startActivity(i);
+
         } else if (id == R.id.nav_slideshow) {
                 Intent i = new Intent(MainActivity.this, MapActivity.class);
                 startActivity(i);
